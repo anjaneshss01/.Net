bf20c98b39ad76e1287a9ebe30f87a5ad71a1c75
Anjanesh Surendraprakash <anjanesh.surendraprakash@aspect.com>
2019-04-02 19:01:43 +0530

    Deligates

---
+++ Deligates/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deligates
+{
+    public delegate void CalcDeligate(int a, int b);
+    class Calculator
+    {
+        //public int a = 0;
+        //public int b = 0;
+
+        public void add(int a, int b)
+        {
+            Console.WriteLine("The Sum is {0}", a + b);
+        }
+
+        public void sub(int a, int b)
+        {
+            Console.WriteLine("The Difference is {0}", a - b);
+        }
+        public void Mul(int a, int b)
+        {
+            Console.WriteLine("The Product is {0}", a * b);
+        }
+        public void Div(int a, int b)
+        {
+            Console.WriteLine("The Division is {0}", a / b);
+        }
+    }
+}
---
+++ Deligates/DeligateMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deligates
+{
+    class DeligateMain
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            CalcDeligate cd = new CalcDeligate(c.add);
+
+            cd.BeginInvoke(10,20);
+        }
+
+        //static void GetNumber()
+        //{
+        //    Console.WriteLine("Enter the 2 numbers");
+        //    int a = Convert.ToInt16(Console.ReadLine());
+        //    int b = Convert.ToInt16(Console.ReadLine());
+        //}
+    }
+}
