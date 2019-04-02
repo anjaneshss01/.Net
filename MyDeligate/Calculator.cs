17bda2097cbaabd232ba1b0fb2641a89b7b558c0
Anjanesh S S <anjaneshss@hotmail.com>
2019-04-02 21:49:39 +0530

    Deligates

---
+++ MyDeligate/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDeligate
+{
+    public delegate void CalcDelegate(int a, int b); // Deligate Declaration
+
+    public class Calculator
+    {
+        public void Add(int a,int b)
+        {
+            Console.WriteLine("The Sum is {0}", a + b);
+        }
+
+        public void Sub(int a,int b)
+        {
+            Console.WriteLine("The Difference is {0}", a - b);
+        }
+
+        public void Mul(int a,int b)
+        {
+            Console.WriteLine("The Product is {0}", a * b);
+        }
+
+        public void Div(int a,int b)
+        {
+            Console.WriteLine("The Division is {0}", a / b);
+        }
+    }
+}
---
+++ MyDeligate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDeligate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            CalcDelegate cd = new CalcDelegate(c.Add); // SingleCast Deligate Instantition
+
+            cd += new CalcDelegate(c.Sub); // Multicast Deligate
+            cd += new CalcDelegate(c.Mul);
+            
+            cd(0,3); // Deligate Invocations
+
+            Console.WriteLine("----------------New Deligate with Division----------------");
+            CalcDelegate cdd = new CalcDelegate(c.Div);
+            cdd(20, 10);
+
+            Console.Read();
+        }
+    }
+}
